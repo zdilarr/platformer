74723fe5e46020a9b0fc4d112a79e9b85181b3e2
zdilarr <zdilar.emilija@hotmail.com>
2024-03-31 17:53:28 +0200

    WIP: Player rotation fixed

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,9 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimatedSprite2D")
 
+func _ready():
+	anim.play("Idle")
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -21,7 +24,9 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.play("Run")
 	else:
+		anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
